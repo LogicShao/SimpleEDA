4612394d6d790d74cb274af89d4d537beca16517
logicshao <1712207189@qq.com>
2024-12-29 23:54:43 +0800

    绘制电容电感

--- CircuitItem/BasicItem.py
+++ CircuitItem/BasicItem.py
@@ -165,3 +165,159 @@ class ResistorItem(BaseCircuitItem):
         self.resistance = value
         self.resistanceInfo.set_text('{:.0f}Ω'.format(self.resistance))
         self.scene().update()
+
+
+class CapacitorSymbol(ItemSymbol):
+    def __init__(self, parent: qtw.QGraphicsItem):
+        super().__init__(parent=parent)
+        self.width = 60
+        self.height = 20
+
+    def boundingRect(self):
+        return qtc.QRectF(0, 0, self.width, self.height)
+
+    def paint(self, painter, option, widget=None):
+        pen = qtg.QPen(qtc.Qt.GlobalColor.red, 2)
+        painter.setPen(pen)
+        # 绘制两条竖线
+        painter.drawLine(qtc.QPointF(0, 0), qtc.QPointF(0, self.height))
+        painter.drawLine(qtc.QPointF(self.width, 0),
+                         qtc.QPointF(self.width, self.height))
+        # 绘制两条横线
+        painter.drawLine(qtc.QPointF(0, self.height / 2),
+                         qtc.QPointF(self.width, self.height / 2))
+
+    def getNodesPos(self) -> list[qtc.QPointF]:
+        # 获取电容两端的节点
+        return [qtc.QPointF(0, self.height / 2),
+                qtc.QPointF(self.width, self.height / 2)]
+
+
+class CapacitorItem(BaseCircuitItem):
+    _item_counter = ItemCounter()
+    _has_value = True
+
+    @staticmethod
+    def What() -> str:
+        return '电容'
+
+    def __init__(self, capacitance: float = 10):
+        super().__init__()
+
+        self.mainSymbol = CapacitorSymbol(parent=self)
+        self.nodes = [ItemNode(parent=self, position=pos)
+                      for pos in self.mainSymbol.getNodesPos()]
+
+        self.width = self.mainSymbol.width + 2 * self.nodes[0].radius
+        self.height = self.mainSymbol.height
+
+        self.capacitance = capacitance
+        self.capacitanceInfo = ItemInfo(
+            parent=self,
+            text='{:.0f}F'.format(self.capacitance),
+            position=qtc.QPointF(self.width / 2, -12)
+        )
+
+        self.nameText = ItemInfo(
+            parent=self,
+            text=self.getName(),
+            position=qtc.QPointF(self.width / 2, self.height + 14)
+        )
+
+    def paint(self, painter, option, widget=None):
+        pass
+
+    def boundingRect(self):
+        return qtc.QRectF(0, 0, self.width, self.height)
+
+    def set_value(self, value):
+        self.capacitance = value
+        self.capacitanceInfo.set_text('{:.0f}F'.format(self.capacitance))
+        self.scene().update()
+
+
+class InductorSymbol(ItemSymbol):
+    def __init__(self, parent: qtw.QGraphicsItem):
+        super().__init__(parent=parent)
+        self.width = 60
+        self.height = 20
+
+    def getPoints(self) -> list[qtc.QPointF]:
+        points = []
+        # 左边起点
+        points.append(qtc.QPointF(0, self.height / 2))
+        sideLineWidth = 10
+        # 绘制折线
+        zigzag_cnt = 8
+        zigzag_dx = (self.width - 2 * sideLineWidth) / zigzag_cnt
+        zigzag_dy = self.height / 2
+        begin_x = 0 + sideLineWidth
+        begin_y = self.height / 2
+        points.append(qtc.QPointF(begin_x, begin_y))
+        for i in range(1, zigzag_cnt):
+            x = begin_x + i * zigzag_dx
+            y = begin_y + zigzag_dy if i % 2 == 0 else begin_y - zigzag_dy
+            points.append(qtc.QPointF(x, y))
+        points.append(qtc.QPointF(self.width - sideLineWidth, self.height / 2))
+        # 右边终点
+        points.append(qtc.QPointF(self.width, self.height / 2))
+        return points
+
+    def boundingRect(self):
+        return qtc.QRectF(0, 0, self.width, self.height)
+
+    def paint(self, painter, option, widget=None):
+        pen = qtg.QPen(qtc.Qt.GlobalColor.red, 2)
+        painter.setPen(pen)
+        # 绘制 zig-zag 线（电感符号）
+        points = self.getPoints()
+        for point1, point2 in zip(points[:-1], points[1:]):
+            painter.drawLine(point1, point2)
+
+    def getNodesPos(self) -> list[qtc.QPointF]:
+        # 获取电感两端的节点
+        return [qtc.QPointF(0, self.height / 2),
+                qtc.QPointF(self.width, self.height / 2)]
+
+
+class InductorItem(BaseCircuitItem):
+    _item_counter = ItemCounter()
+    _has_value = True
+
+    @staticmethod
+    def What() -> str:
+        return '电感'
+
+    def __init__(self, inductance: float = 10):
+        super().__init__()
+
+        self.mainSymbol = InductorSymbol(parent=self)
+        self.nodes = [ItemNode(parent=self, position=pos)
+                      for pos in self.mainSymbol.getNodesPos()]
+
+        self.width = self.mainSymbol.width + 2 * self.nodes[0].radius
+        self.height = self.mainSymbol.height
+
+        self.inductance = inductance
+        self.inductanceInfo = ItemInfo(
+            parent=self,
+            text='{:.0f}H'.format(self.inductance),
+            position=qtc.QPointF(self.width / 2, -12)
+        )
+
+        self.nameText = ItemInfo(
+            parent=self,
+            text=self.getName(),
+            position=qtc.QPointF(self.width / 2, self.height + 14)
+        )
+
+    def paint(self, painter, option, widget=None):
+        pass
+
+    def boundingRect(self):
+        return qtc.QRectF(0, 0, self.width, self.height)
+
+    def set_value(self, value):
+        self.inductance = value
+        self.inductanceInfo.set_text('{:.0f}H'.format(self.inductance))
+        self.scene().update()
--- CircuitItem/__init__.py
+++ CircuitItem/__init__.py
@@ -1,10 +1,10 @@
 from .BaseCircuitItem import BaseCircuitItem, ItemNode, CircuitNode
 from .SourceItem import VoltageSourceItem, GroundItem, CurrentSourceItem
-from .BasicItem import ResistorItem, WireItem
+from .BasicItem import ResistorItem, WireItem, CapacitorItem, InductorItem
 from .MeterItem import VoltmeterItem, AmmeterItem
 from .CircuitTopology import CircuitTopology
 
 __all__ = ['BaseCircuitItem', 'ItemNode', 'VoltageSourceItem', 'GroundItem',
-           'CurrentSourceItem', 'ResistorItem', 'WireItem', 'VoltmeterItem', 'AmmeterItem', 'CircuitNode', 'CircuitTopology']
+           'CurrentSourceItem', 'ResistorItem', 'WireItem', 'VoltmeterItem', 'AmmeterItem', 'CircuitNode', 'CircuitTopology', 'CapacitorItem', 'InductorItem']
 BTN_ITEM_TYPES = [VoltageSourceItem, GroundItem,
-                  CurrentSourceItem, ResistorItem, VoltmeterItem, AmmeterItem]
+                  CurrentSourceItem, ResistorItem, CapacitorItem, InductorItem]
